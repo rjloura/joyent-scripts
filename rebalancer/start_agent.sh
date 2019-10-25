2f9af47767628ef1fe4e608ea44a16f6c8e02ab8
Rui Loura <rui@joyent.com>
2019-10-25 09:31:43 -0400

    add comments to start_agent.sh

--- rebalancer/start_agent.sh
+++ rebalancer/start_agent.sh
@@ -1,3 +1,7 @@
+## This script should be uploaded with update_agent_on_storage_nodes.sh
+## 
+## Run this with `manta-oneach -s storage '/var/tmp/start_agent.sh'`
+##
 export LD_LIBRARY_PATH=/opt/local/lib/amd64/
 RUST_LOG=remora=debug nohup /var/tmp/remora --agent -c /var/tmp/config.json &
 
